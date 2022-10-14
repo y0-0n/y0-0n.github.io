a2f1fcf8dde222878dbc9e2cda72eb3ebbea2be7
l5vd5 <l5vd5@korea.ac.kr>
2022-10-14 12:54:30 +0900

    fix github repo url

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,9 +4,7 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
-      Gatsby-starter-bee
-    </a>
+    ©<a href="https://github.com/l5vd5">Jbee</a>, Built with{' '}
+    <a href="https://github.com/l5vd5">Gatsby-starter-bee</a>
   </footer>
 )
